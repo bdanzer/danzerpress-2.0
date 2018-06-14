2e769c93078c025368b35349ea83ddd73707e5f1
bdanzer <britt.danzer@gmail.com>
2018-06-13 23:59:17 -0500

    added click to scroll and added section scss

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,30 @@
         $(document).ready(function() {
           $('.drawer').drawer();
         });
+
+        //Scrolling to anchor
+        $(".danzerpress-hash").on('click', function(event) {
+
+          // Make sure this.hash has a value before overriding default behavior
+          if (this.hash !== "") {
+              // Prevent default anchor click behavior
+              event.preventDefault();
+
+              // Store hash
+              var hash = this.hash;
+              console.log(hash);
+
+              // Using jQuery's animate() method to add smooth page scroll
+              // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+              $('html, body').animate({
+                scrollTop: $(hash).offset().top + (-64)
+              }, 1000, function(){
+           
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                
+              });
+            } // End if
+        });
         
       },
       finalize: function() {
