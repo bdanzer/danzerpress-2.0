e4a614df98d83c81769b4c1c170fcf537291034f
bdanzer <britt.danzer@gmail.com>
2018-08-24 20:33:15 -0500

    added sidebar to context

--- index.php
+++ index.php
@@ -4,5 +4,6 @@ use Danzerpress\Contexts\DanzerpressPostContext;
 
 $context = Danzerpress::get_context();
 $context['posts'] = Timber::get_posts();
+$context['sidebar_primary'] = Timber::get_widgets('sidebar-primary');
 
 Timber::render('templates/index.twig', $context, Danzerpress::get_ttl());
