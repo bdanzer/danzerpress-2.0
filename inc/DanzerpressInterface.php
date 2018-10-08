841732ac36fcbb913d780f9f304aefe160d78352
bdanzer <britt.danzer@gmail.com>
2018-10-08 13:13:45 -0500

    will need to fix cache buffer later

--- inc/DanzerpressInterface.php
+++ inc/DanzerpressInterface.php
@@ -7,6 +7,6 @@ Class DanzerpressInterface {
         $plugin_url = plugins_url();
         define("DP_PLUGIN_URL", (true === IS_DEV) ? $plugin_url . '/danzerpress-plugin/' : $plugins_url . '/dp-plugin/');
         //wp_get_theme()->parent()->get('Version')
-        define("DP_CACHE_BUFFER", "3.0.0");
+        define("DP_CACHE_BUFFER", null);
     }
 }
