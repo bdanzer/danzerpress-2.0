6ec96e00bae5e9d8cd82cc57b0d869c320389ea5
bdanzer <britt.danzer@gmail.com>
2019-01-06 04:24:01 -0600

    adding safe ways to get dependencies, and added version number

--- inc/autoinstaller/RegisterPlugins.php
+++ inc/autoinstaller/RegisterPlugins.php
@@ -129,7 +129,7 @@ Class RegisterPlugins {
             'parent_slug'  => 'themes.php',            // Parent menu slug.
             'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
             'has_notices'  => true,                    // Show admin notices or not.
-            'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+            'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
             'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
             'is_automatic' => false,                   // Automatically activate plugins after installation or not.
             'message'      => '',                      // Message to output right before the plugins table.
